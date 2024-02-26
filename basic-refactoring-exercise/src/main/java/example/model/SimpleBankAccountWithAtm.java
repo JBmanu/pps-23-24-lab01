aed8d6110285d3137df78c43dbd31d2b0d271cd7
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-02-26 10:33:40 +0100

    feat: create in SimpleBankAccountWithAtm fee

--- basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
@@ -1,11 +1,18 @@
 package example.model;
 
-public class SimpleBankAccountWithAtm implements BankAccount {
+public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankAccount {
+    private static final double FEE_DEFAULT = 1.;
 
+    private double fee;
+
+    public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
+        super(holder, balance);
+        this.fee = FEE_DEFAULT;
+    }
 
     @Override
     public AccountHolder getHolder() {
-        return null;
+        return super.getHolder();
     }
 
     @Override
