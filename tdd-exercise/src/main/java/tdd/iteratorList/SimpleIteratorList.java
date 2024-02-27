4de55f605b274c56d4886247d841d1bfa0ea1aa2
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-02-27 18:17:00 +0100

    feat: green testAddElementInList

--- tdd-exercise/src/main/java/tdd/iteratorList/SimpleIteratorList.java
+++ tdd-exercise/src/main/java/tdd/iteratorList/SimpleIteratorList.java
@@ -1,21 +1,30 @@
 package tdd.iteratorList;
 
+import tdd.SimpleCircularList;
+
 import java.util.Iterator;
+import java.util.Optional;
 
 public class SimpleIteratorList implements IteratorList {
+    private Optional<Integer> element;
+
+    public SimpleIteratorList() {
+        this.element = Optional.empty();
+    }
+
     @Override
     public void add(int element) {
-
+        this.element = Optional.of(element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.element.isEmpty() ? 0 : 1;
     }
 
     @Override
     public boolean isEmpty() {
-        return true;
+        return this.element.isEmpty();
     }
 
     @Override
--- tdd-exercise/src/test/java/SimpleIteratorListTest.java
+++ tdd-exercise/src/test/java/SimpleIteratorListTest.java
@@ -3,11 +3,17 @@ import org.junit.jupiter.api.Test;
 import tdd.iteratorList.IteratorList;
 import tdd.iteratorList.SimpleIteratorList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleIteratorListTest {
     private static final int EMPTY_LIST = 0;
+
+    private static final int ELEMENT_ZERO = 0;
+    private static final int ONE_ELEMENT = 1;
+
+    private static final Optional<Integer> ZERO_OPTIONAL_VALUE = Optional.of(0);
 
     private IteratorList iteratorList = new SimpleIteratorList();
 
@@ -22,6 +28,13 @@ public class SimpleIteratorListTest {
         assertEquals(EMPTY_LIST, this.iteratorList.size());
     }
 
+    @Test
+    public void testAddElementInList() {
+        this.iteratorList.add(ELEMENT_ZERO);
+
+        assertFalse(this.iteratorList.isEmpty());
+        assertEquals(ONE_ELEMENT, this.iteratorList.size());
+    }
 
 
 
