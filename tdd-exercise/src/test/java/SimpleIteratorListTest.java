70b237aef6953d164da5c6908cd132c5ffa56ee7
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-02-27 18:06:47 +0100

    feat: create SimpleIteratorList test

---
+++ tdd-exercise/src/test/java/SimpleIteratorListTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import tdd.iteratorList.IteratorList;
+import tdd.iteratorList.SimpleIteratorList;
+
+public class SimpleIteratorListTest {
+
+    @Test
+    public void canCreateIteratorList() {
+        IteratorList iteratorList = new SimpleIteratorList();
+
+    }
+
+
+}
