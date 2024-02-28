c84fac66b53fc339e535de92a11b6acb43409c18
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-02-28 12:32:26 +0100

    feat: create test for FilterCircularlyList

---
+++ tdd-exercise/src/test/java/FilterCircularlyListTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import tdd.filterCircularlyList.FilterCircularlyList;
+import tdd.filterCircularlyList.SimpleFilterCircularlyList;
+
+public class FilterCircularlyListTest {
+
+    @Test
+    public void canCreateFilterCircularlyTest() {
+        FilterCircularlyList filterCircularlyList = new SimpleFilterCircularlyList();
+    }
+}
