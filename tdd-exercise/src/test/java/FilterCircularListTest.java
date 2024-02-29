b03788bef8a37cbf4745acb2397e28f9e36cf689
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-02-29 14:31:21 +0100

    feat: improv with stream FilterCircularListTest

--- tdd-exercise/src/test/java/FilterCircularListTest.java
+++ tdd-exercise/src/test/java/FilterCircularListTest.java
@@ -5,12 +5,12 @@ import tdd.filterCircularList.SimpleFilterCircularList;
 
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FilterCircularListTest {
     private static final int EMPTY_LIST = 0;
-    private static final int INITIAL_VALUE_OF_FOR = 0;
     private static final int ONE_ELEMENT = 1;
     private static final int THREE_ELEMENT = 3;
     private static final int EIGHT_ELEMENT = 8;
@@ -30,8 +30,7 @@ public class FilterCircularListTest {
     }
 
     private void addElementInOrderInList(final int quantity) {
-        for (int i = INITIAL_VALUE_OF_FOR; i < quantity; i++)
-            this.filterCircularList.add(i);
+        IntStream.range(ZERO_VALUE, quantity).forEach(value -> this.filterCircularList.add(value));
     }
 
     @Test
@@ -56,7 +55,7 @@ public class FilterCircularListTest {
         final int elementZero = 0;
         final Predicate<Integer> predicate = element -> element.equals(elementZero);
 
-        assertEquals(Optional.of(0), this.filterCircularList.filterNext(predicate));
+        assertEquals(Optional.of(ZERO_VALUE), this.filterCircularList.filterNext(predicate));
     }
 
     @Test
@@ -76,40 +75,38 @@ public class FilterCircularListTest {
         final int trueMultiple = 0;
         final Predicate<Integer> predicate = element -> (element % multiple) == trueMultiple;
 
-        for (int i = INITIAL_VALUE_OF_FOR; i < THREE_ELEMENT; i++)
-            assertEquals(Optional.of(i * multiple), this.filterCircularList.filterNext(predicate));
+        IntStream.range(ZERO_VALUE, THREE_ELEMENT)
+                .map(value -> value * multiple)
+                .forEach(value -> assertEquals(Optional.of(value), this.filterCircularList.filterNext(predicate)));
     }
 
     @Test
     public void testTwoDifferentFilterNextInOrder() {
         this.addElementInOrderInList(EIGHT_ELEMENT);
 
+        final int multipleOfThree = 3;
         final int multipleOfTwo = 2;
         final int trueMultiple = 0;
         final Predicate<Integer> firstPredicate = element -> (element % multipleOfTwo) == trueMultiple;
-
-        for (int i = INITIAL_VALUE_OF_FOR; i < THREE_ELEMENT; i++)
-            assertEquals(Optional.of(i * multipleOfTwo),
-                    this.filterCircularList.filterNext(firstPredicate));
-
-        final int multipleOfThree = 3;
         final Predicate<Integer> secondPredicate = element -> (element % multipleOfThree) == trueMultiple;
 
-        for (int i = INITIAL_VALUE_OF_FOR; i < THREE_ELEMENT; i++) {
-            assertEquals(Optional.of(i * multipleOfThree),
-                    this.filterCircularList.filterNext(secondPredicate));
-        }
+        IntStream.range(ZERO_VALUE, THREE_ELEMENT)
+                .map(value -> value * multipleOfTwo)
+                .forEach(value -> assertEquals(Optional.of(value), this.filterCircularList.filterNext(firstPredicate)));
+
+        IntStream.range(ZERO_VALUE, THREE_ELEMENT)
+                .map(value -> value * multipleOfThree)
+                .forEach(value -> assertEquals(Optional.of(value), this.filterCircularList.filterNext(secondPredicate)));
     }
 
     @Test
     public void testTwoDifferentFilterNextInDisorder() {
         this.addElementInOrderInList(EIGHT_ELEMENT);
 
+        final int multipleOfThree = 3;
         final int multipleOfTwo = 2;
         final int trueMultiple = 0;
         final Predicate<Integer> firstPredicate = element -> (element % multipleOfTwo) == trueMultiple;
-
-        final int multipleOfThree = 3;
         final Predicate<Integer> secondPredicate = element -> (element % multipleOfThree) == trueMultiple;
 
         assertEquals(Optional.of(ZERO_VALUE), this.filterCircularList.filterNext(firstPredicate));
